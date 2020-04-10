7419a23a2d00b32cf2541392f5eda7df8e88db55
begaonnuri <turtlenam0928@gmail.com>
2020-04-10 13:40:37 +0900

    feat: 원하는 기능을 입력받는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,10 +3,15 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
-    private static final Scanner scanner = new Scanner(System.in);
+	private static final Scanner scanner = new Scanner(System.in);
 
-    public static int inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
-    }
+	public static String inputFunctionNumber() {
+        System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.nextLine();
+	}
+
+	public static int inputTableNumber() {
+		System.out.println("## 주문할 테이블을 선택하세요.");
+		return scanner.nextInt();
+	}
 }
