4bdd568e3a30836c7ce7fadcd9fe6c4bef77c7e1
begaonnuri <turtlenam0928@gmail.com>
2020-04-10 16:15:34 +0900

    feat: 테이블에 주문한 메뉴를 추가하는 기능 구현

--- src/main/java/domain/table/Table.java
+++ src/main/java/domain/table/Table.java
@@ -9,11 +9,23 @@ import domain.menu.MenuRepository;
 
 public class Table {
 	private final TableNumber number;
+	private final HashMap<Menu, MenuAmount> bill;
 
 	public Table(final int number) {
 		this.number = TableNumber.of(number);
+		this.bill = new HashMap<>();
 	}
+
+	public Table(TableNumber number, HashMap<Menu, MenuAmount> bill) {
 		this.number = number;
+		this.bill = new HashMap<>(bill);
+	}
+
+	public Table addMenu(MenuNumber menuNumber, MenuAmount menuAmount) {
+		HashMap<Menu, MenuAmount> bill = new HashMap<>();
+		Menu menu = MenuRepository.findByMenuNumber(menuNumber);
+		bill.put(menu, menuAmount);
+		return new Table(number, bill);
 	}
 
 	public boolean isSameTableNumber(TableNumber tableNumber) {
@@ -22,6 +34,9 @@ public class Table {
 
 	@Override
 	public String toString() {
-		return Integer.toString(number);
+		return "Table{" +
+				"number=" + number +
+				", bill=" + bill +
+				'}';
 	}
 }
