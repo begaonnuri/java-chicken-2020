4f34e7f68be5ffa4bf76a5ca3d5007521941174e
begaonnuri <turtlenam0928@gmail.com>
2020-04-10 15:43:55 +0900

    refactor: Menu 클래스 변경
    - number의 타입을 int에서 MenuNumber로 변경
    - getter, equals, hashCode 추가

--- src/main/java/domain/menu/Menu.java
+++ src/main/java/domain/menu/Menu.java
@@ -1,16 +1,40 @@
 package domain.menu;
 
+import java.util.Objects;
+
 public class Menu {
-	private final int number;
+	private final MenuNumber number;
 	private final String name;
 	private final Category category;
 	private final int price;
 
 	public Menu(final int number, final String name, final Category category, final int price) {
-		this.number = number;
+		this.number = MenuNumber.of(number);
 		this.name = name;
 		this.category = category;
 		this.price = price;
+	}
+
+	public MenuNumber getNumber() {
+		return number;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Menu menu = (Menu)o;
+		return price == menu.price &&
+				Objects.equals(number, menu.number) &&
+				Objects.equals(name, menu.name) &&
+				category == menu.category;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(number, name, category, price);
 	}
 
 	@Override
