8c9833eac1cd80b670668888c260a84ab55b4c91
begaonnuri <turtlenam0928@gmail.com>
2020-04-10 14:27:58 +0900

    feat: 등록할 메뉴를 선택하는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,12 +6,17 @@ public class InputView {
 	private static final Scanner scanner = new Scanner(System.in);
 
 	public static String inputFunctionNumber() {
-        System.out.println("## 원하는 기능을 선택하세요.");
-        return scanner.nextLine();
+		System.out.println("## 원하는 기능을 선택하세요.");
+		return scanner.nextLine();
 	}
 
 	public static int inputTableNumber() {
 		System.out.println("## 주문할 테이블을 선택하세요.");
 		return scanner.nextInt();
+	}
+
+	public static String inputMenuNumber() {
+		System.out.println("## 등록할 메뉴를 선택하세요.");
+		return scanner.nextLine();
 	}
 }
